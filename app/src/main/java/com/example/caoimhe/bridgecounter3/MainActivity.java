5e5e4cfcedb49318b86c90ef32f6069c2ac26a0c
Caoimhe Tiernan <caoimhe.tiernan.work@gmail.com>
2018-11-08 12:20:36 +0000

    Two classes: - Scoreboard contains basic methods for scorekeeping and checking if a game has been won.
    -Team. This inherits from Scoreboard.
     The class will let us define We and Them. A central part of bridge scorekeeping.
    also allow ussers to enter their names

--- app/src/main/java/com/example/caoimhe/bridgecounter3/MainActivity.java
+++ app/src/main/java/com/example/caoimhe/bridgecounter3/MainActivity.java
@@ -10,4 +10,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /* class that encapsulates the
+    scoring
+     */
+    public class Scoreboard
+    {
+        private int upperScore; //the premium score. I don't play bridge this is to help me with the positioning
+        private int underScore; // the trick score.
+
+
+        public Scoreboard() //scoreboard constructor. New round all scores set to 0
+        {
+            upperScore = 0;
+            underScore = 0;
+        }
+
+        public int getUpperScore() //returns upper score
+        {
+            return upperScore;
+        }
+
+        public int getUnderScore() //returns underscore
+        {
+            return underScore;
+        }
+
+
+        public boolean GameWon(int upperScore) //checks if team's upperscore >= 100
+        {   //TODO
+            if(this.upperScore >= 100)
+                return true;
+            else
+                return false;
+        }
+
+
+
+    }
+
+    public class Team extends Scoreboard
+    {
+        private String player1;
+        private String player2;
+
+    }
 }
+
